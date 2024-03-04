151eb3762759709f4676f097f22d6f2f4e5ea851
goodmanjosephd <65240635+goodmanjosephd@users.noreply.github.com>
2024-03-04 07:48:15 -0500

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -1,11 +1,12 @@
-const config = {
+@type {import("../config/siteConfig").UserConfig}
+
+export default {
   title: "Joe's Notes",
   description: "This is my awesome blog built with Flowershow",
   author: "Joe",
   logo: "/images/logo.svg",
-  domain: "https://notes.joesmusings.com",
+  domain: "https://notes.joesmusings.com/",
   // links to the pages you want to link to in the navbar and in the footer
   navLinks: [{ href: "/about", name: "About" }],
+  
 };
-
-export default config;
