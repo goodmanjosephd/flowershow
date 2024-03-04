c698d65df2d4fdee437c2ffa55e4a6c734023111
goodmanjosephd <65240635+goodmanjosephd@users.noreply.github.com>
2024-03-04 07:27:50 -0500

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -1,12 +1,32 @@
-
-/**
- * @type {import("../config/siteConfig").UserConfig}
- */
-
-const userConfig = {
-  navbarTitle: {
-    text: "Joe's Notes"
+export const defaultConfig = {
+  title: "Joe's Notes",
+  description: "",
+  showEditLink: false,
+  showToc: true,
+  showSidebar: true,
+  showLinkPreviews: true,
+  author: "",
+  authorLogo: "",
+  domain: "",
+  // Google analytics key e.g. G-XXXX
+  analytics: "",
+  // content source directory for markdown files
+  // DO NOT CHANGE THIS VALUE
+  // if you have your notes in another (external) directory,
+  // /content dir should be a symlink to that directory
+  content: "content",
+  avatarPlaceholder: "/_flowershow/avatarplaceholder.png",
+  contentExclude: [],
+  contentInclude: [],
+  blogDir: "blog",
+  peopleDir: "people",
+  // Theme
+  theme: {
+    default: "dark",
+    toggleIcon: "/_flowershow/theme-button.svg",
   },
+  navLinks: [
+    { href: '/index', name: 'Home' },
+    { href: 'https://joesmusings.com', name: 'Blog' },
+  ],
 };
-
-export default {};
