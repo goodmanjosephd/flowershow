c85faa0a9bb28cfc7cf1fcc633e6d9f353a52f70
Joe <65240635+joesmusings@users.noreply.github.com>
2024-03-16 19:40:35 -0400

    Update SourceLayout.tsx

--- layouts/SourceLayout.tsx
+++ layouts/SourceLayout.tsx
@@ -10,11 +10,11 @@ export default function SourceLayout({ children, ...frontMatter }) {
     <article className="docs prose prose-a:text-primary dark:prose-a:text-primary-dark prose-strong:text-primary dark:prose-strong:text-primary-dark prose-code:text-primary dark:prose-code:text-primary-dark prose-headings:text-primary dark:prose-headings:text-primary-dark text-primary dark:text-primary-dark prose-headings:font-headings dark:prose-invert prose-a:break-words mx-auto p-6">
       <header>
         <div className="container mx-auto">
-            {author && <p>{author}</p>}
-            {url && <p>{url}</p>}
-            {status && <p>{status}</p>}
-            {type && <p>{type}</p>}
-            {dateCreated && <p>{dateCreated}</p>}
+            {author && <p>by: {author}</p>}
+            {url && <p>link: {url}</p>}
+            {status && <p>status: {status}</p>}
+            {type && <p>type: {type}</p>}
+            {dateCreated && <p> Date Created: {dateCreated}</p>}
         </div>
         </header>
         <section>{children}</section>
